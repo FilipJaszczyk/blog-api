962540c595d0b39de65afcdc328319d22b625993
Filip Jaszczyk <filipqwerty32@gmail.com>
2020-04-03 13:34:43 +0200

    token auth with simple JWT

--- blogProject/blogApp/tests.py
+++ blogProject/blogApp/tests.py
@@ -67,120 +67,27 @@ class PostListPostDetailTest(APITestCase):
         response = self.client.delete(reverse('post_detail', kwargs={'pk': self.test_user_0_post.pk}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    
-
-# ========= OLD TEST FOR NO AUTHENTICATION ==================
-# class PostTestCreateMethod(APITestCase):
-#     def test_create_post_unauthenticated(self):
-#         '''
-#         Ensure we can create new post
-#         '''
-#         url = reverse('posts')
-#         data = {'title': 'Post created', 'content' : 'Post created sucessfully'}
-#         response = self.client.post(url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-
-# class PostTestGetMethod(APITestCase):
-#     def setup(self):
-#         Post.objects.create(title="POST CREATED", content="POST WAS CREATED")
-#         Post.objects.create(title="POST CREATED 2", content="POST WAS CREATED 2")
-#         Post.objects.create(title="POST CREATED 3", content="POST WAS CREATED 3")
-
-#     def test_get_posts(self):
-#         '''
-#         Ensure we can get list of posts
-#         '''
-#         # get API response 
-#         response = self.client.get(reverse('posts'))
-#         # get data from DB
-#         posts = Post.objects.all()
-#         # convert it to JSON
-#         serializer = PostSerializer(posts, many=True)
-#         # check the status 
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         #check if data is same 
-#         self.assertEqual(response.data, serializer.data)
-        
-# class PostTestPutMethod(APITestCase):
-#     def setUp(self):
-#         # Posts to be modified 
-#         self.first_post = Post.objects.create(title="POST CREATED", content="POST WAS CREATED")
-#         self.second_post = Post.objects.create(title="POST CREATED 2", content="POST WAS CREATED 2")
-#         #url
-#         self.url = 'post_detail'
-#         # modifications
-#         self.valid_update_post = {
-#             "title" : "post is changed",
-#             "content": "post is changed"
-#         }
-#         self.invalid_update_post = {
-#             "title": "",
-#             "content": "post change"
-#         }
-#         self.valid_partial_update_post = {
-#             "title" : "changed title"
-#         }
-
-#         self.invalid_partial_update_post = {
-#             "title" : "",
-#             "content" : "only content changed"
-#         }
-
-#     def test_valid_update_post(self):
-#         '''
-#         Validated data case for PUT METHOD 
-#         '''
-#         response = self.client.put(
-#             reverse(self.url, kwargs={'pk': self.first_post.pk}),
-#             data = json.dumps(self.valid_update_post),
-#             content_type = 'application/json'
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-#     def test_invalid_update_post(self):
-#         '''
-#         Invalid data case PUT METHOD 
-#         '''
-#         response = self.client.put(
-#             reverse(self.url, kwargs={'pk': self.second_post.pk}),
-#             data = json.dumps(self.invalid_update_post),
-#             content_type = 'application/json'
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-#     def test_valid_partial_update_post(self):
-#         '''
-#         Valid data case PATCH METHOD 
-#         '''
-#         response = self.client.patch(
-#             reverse(self.url, kwargs={'pk': self.first_post.pk}),
-#             data = json.dumps(self.valid_partial_update_post),
-#             content_type = 'application/json'
-#         )
-#         requested_title = self.valid_partial_update_post['title']
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response.data['title'], requested_title)
-
-#     def test_invalid_partial_update_post(self):
-#         '''
-#         Invalid data case PATCH METHOD 
-#         '''
-#         response = self.client.patch(
-#             reverse(self.url, kwargs={'pk': self.first_post.pk}),
-#             data = json.dumps(self.invalid_partial_update_post),
-#             content_type = 'application/json'
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-# class UserTestRegisterMethod(APITestCase):
-#     def test_create_valid_user(self):
-        url = reverse('register_user')
-        data = {
-            'username': 'username',
-            'email': 'email@email.com',
-            'password': 'password'
+    def test_get_token_update_post(self):
+        '''
+        User is obtaining a token and then updates post 
+        '''
+        user_data = {
+            "username" : "tester",
+            "password" : "tester"
         }
+        patch_data = {
+            "content" : "content changed"
+        }
+        token_response = self.client.post(reverse('token_obtain_pair'), json.dumps(user_data), content_type='application/json')
+        token = token_response.data['access']
+        auth_header = "Bearer {}".format(token)
 
-        response = self.client.post(url, data=json.dumps(data), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.client.credentials(HTTP_AUTHORIZATION= auth_header)
+        
+        response = self.client.patch(
+            reverse('post_detail', kwargs={'pk': self.test_user_0_post.pk}), 
+            json.dumps(patch_data), 
+            content_type = 'application/json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
--- blogProject/blogApp/views.py
+++ blogProject/blogApp/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import generics, permissions
 from rest_framework import filters
 from .models import Post, User
-from .serializers import PostSerializer, UserSerializer, UserPostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
@@ -34,53 +34,8 @@ class UserRegistration(generics.CreateAPIView):
     '''
     serializer_class = UserSerializer
 
-class SearchList(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['author__username', 'title']    
+
     
 
 
-'''
-Function based views
-'''
-# Create your views here.
-# @api_view(['GET', 'POST'])
-# def post_list(request, format=None):
-#     if request.method == 'GET':
-#         posts = Post.objects.all()
-#         serializer = PostSerializer(posts, many=True)
-#         return Response(serializer.data)
 
-#     elif request.method == 'POST':
-#         serializer = PostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def post_detail(request, pk, format=None):
-#     """
-#     Retrieve, update or delete a post.
-#     """
-#     try:
-#         post = Post.objects.get(pk=pk)
-#     except Post.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-#     # get single view
-#     if request.method == 'GET':
-#         serializer = PostSerializer(post)
-#         return Response(data=serializer.data, status=status.HTTP_200_OK)
-#     # update single view
-#     elif request.method == 'PUT':
-#         serializer = PostSerializer(post, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     # delete view
-#     elif request.method == 'DELETE':
-#         post.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
